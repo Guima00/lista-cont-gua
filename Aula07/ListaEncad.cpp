909d9187469e05524194ba490f7f9c6b105f5b32
Guima <48738309+Guima00@users.noreply.github.com>
2019-05-13 20:48:31 -0300

    Add files via upload

---
+++ Aula07/ListaEncad.cpp
@@ -0,0 +1,177 @@
+#include <iostream>
+#include <cstdlib>
+#include "ListaEncad.h"
+
+using namespace std;
+
+ListaEncad::ListaEncad()
+{
+    cout << "Criando objeto ListaEncad" << endl;
+    primeiro = NULL;
+    ultimo = NULL;
+    n = 0;
+}
+
+ListaEncad::~ListaEncad()
+{
+    cout << "Destruindo objeto ListaEncad" << endl;
+    No *p = primeiro;
+    while(p != NULL)
+    {
+        No *t = p->getProx();
+        delete p;
+        p = t;
+    }
+}
+
+bool ListaEncad::busca(int val)
+{
+    No *p;
+    for(p = primeiro; p != NULL; p = p->getProx())
+        if(p->getInfo() == val)
+            return true;
+    return false;
+}
+
+int ListaEncad::get(int k)
+{
+    No *p = primeiro;
+    int i = 0;
+    while(i < k && p != NULL)
+    {
+        i++;
+        p = p->getProx();
+    }
+    if(p == NULL)
+    {
+        cout << "ERRO: Indice invalido!" << endl;
+        exit(1);
+    }
+    else
+        return p->getInfo();
+}
+
+void ListaEncad::set(int k, int val)
+{
+    No *p = primeiro;
+    int i = 0;
+    while(i < k && p != NULL)
+    {
+        i++;
+        p = p->getProx();
+    }
+    if(p == NULL)
+        cout << "ERRO: Indice invalido!" << endl;
+    else
+        p->setInfo(val);
+}
+
+
+void ListaEncad::insereInicio(int val)
+{
+    No* p = new No();
+    p->setInfo(val);
+    p->setProx(primeiro);
+
+    primeiro = p;
+
+    n++;
+    if(n == 1) ultimo = p;
+}
+
+void ListaEncad::insereFinal(int val)
+{
+    No* p = new No();
+    p->setInfo(val);
+    p->setProx(NULL);
+
+    if(ultimo != NULL) ultimo->setProx(p);
+    ultimo = p;
+
+    n++;
+    if(n == 1) primeiro = p;
+}
+
+void ListaEncad::removeInicio()
+{
+    No* p;
+    if(primeiro != NULL)
+    {
+        p = primeiro;
+        primeiro = p->getProx();
+        delete p;
+
+        n--;
+        if(n == 0) ultimo = NULL;
+    }
+    else
+        cout << "ERRO: lista vazia!" << endl;
+}
+
+void ListaEncad::removeFinal()
+{
+    No* p;
+    if(ultimo != NULL)
+    {
+        if(primeiro == ultimo)
+        {
+            primeiro = NULL;
+            p = NULL;
+        }
+        else
+        {
+            p = primeiro;
+            while(p->getProx() != ultimo)
+                p = p->getProx();
+            p->setProx(NULL);
+        }
+        delete ultimo;
+        ultimo = p;
+        n--;
+    }
+    else
+        cout << "ERRO: lista vazia!" << endl;
+
+}
+void ListaEncad::imprime(){
+     No* aux;
+     if(primeiro!=NULL){
+       for(aux=primeiro;aux!=NULL;aux=aux->getProx())
+       {
+           cout<<aux->getInfo()<<" ";
+        }
+        cout<<endl;
+     }
+}
+int ListaEncad::numnos(){
+    return n;
+}
+int ListaEncad::buscaMaior(int val){
+    No* p;
+    int cont=0;
+    for(p=primeiro;p!=NULL;p=p->getProx(),cont++)
+    {
+        if(p->getInfo()>val)
+           return cont;
+    }
+    return -1;
+}
+void ListaEncad::limpar(){
+     No* p;
+     if(n!=0)
+     {
+        while(n!=0){
+            removeInicio();
+        }
+     }
+}
+float ListaEncad::calculaMedia(){
+      float media=0;
+      No* p;
+      for(p=primeiro;p!=0;p=p->getProx())
+      {
+         media=media+p->getInfo();
+      }
+      return media/n;
+}
+void ListaEncad::concatena(ListaEncad *l2);
